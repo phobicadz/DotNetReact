47e5fd552838f1465b4493e02254fc10e5c50725
Chamberlain <Adam.Chamberlain@kier.co.uk>
2018-12-04 16:37:01 +0000

    .

--- ClientApp/components/RandomUsers.tsx
+++ ClientApp/components/RandomUsers.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link, RouteComponentProps } from 'react-router-dom';
+//import { Link, RouteComponentProps } from 'react-router-dom';
 
 interface randomUserState {
   pictures: any,
